ae342db4c50f873f41d91e5a5a66e745ee3d42e6
kexogg <kexogg@gmail.com>
2024-12-04 03:12:08 +0500

    remove old tests

--- cs/MarkdownTests/MdTests.cs
+++ cs/MarkdownTests/MdTests.cs
@@ -25,37 +25,4 @@ public class MdTests
     {
         md.Render("Hello, _world_!").Should().Be("Hello, <em>world</em>!");
     }
-
-    [TestCase("Hello, __world__!", "Hello, <strong>world</strong>!", TestName = "Simple strong")]
-    [TestCase("Hello, world! ____", "Hello, world! ____", TestName = "Empty strong")]
-    public void Render_ShouldReturnStrongText(string input, string expected)
-    {
-        md.Render(input).Should().Be(expected);
-    }
-
-    [Test]
-    public void Render_ShouldReturnHeadingText()
-    {
-        md.Render("#Hello, world!").Should().Be("<h1>Hello, world!</h1>");
-    }
-
-    [Test]
-    public void Render_ShouldReturnImage()
-    {
-        md.Render("![Hello, image!](img)").Should().Be("<img alt=\"Hello, image!\" src=\"img\"/>");
-    }
-
-    [Test]
-    public void Render_ShouldEscapeSpecialCharacters()
-    {
-        md.Render(@"Hello, \_world\_!").Should().Be("Hello, _world_!");
-    }
-
-    [Test]
-    [TestCase("#Hello, __world__!", "<h1>Hello, <strong>world!</strong></h1>")]
-    [TestCase("__Hello, _world_!__", "<strong>Hello, <em>world</em>!</strong>")]
-    public void Render_ShouldReturnNestedFormatting(string input, string expected)
-    {
-        md.Render(input).Should().Be(expected);
-    }
 }
